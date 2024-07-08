642125eb10109a20552b8a51452a6ffdee7d7cf1
Brian <kingbros@msn.com>
2024-07-08 15:48:11 -0700

    first commit

---
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class ShootCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Shooter shooter;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ShootCommand(Shooter subsystem) {
+    shooter = subsystem;
+
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    shooter.initializeShoot();
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+
+  boolean finish = false;
+  @Override
+  public void execute() {
+    finish = shooter.runShooterMotors();
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooter.resetMotors();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if(finish) {
+      return true;
+    }
+    return false;
+
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,123 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  CANSparkFlex intakeMotor = new CANSparkFlex(32, MotorType.kBrushless);
+  CANSparkFlex upperShooterMotor = new CANSparkFlex(30, MotorType.kBrushless);
+  CANSparkFlex lowerShooterMotor = new CANSparkFlex(31, MotorType.kBrushless);
+  CANSparkMax leftRotateMotor = new CANSparkMax(28, MotorType.kBrushless);
+  CANSparkMax rightRotateMotor = new CANSparkMax(29, MotorType.kBrushless);
+  RelativeEncoder upperEncoder = upperShooterMotor.getEncoder();
+  RelativeEncoder lowerEncoder = lowerShooterMotor.getEncoder();
+  private static Shooter shooter;
+  Timer timer = new Timer();
+  boolean started = false;
+  boolean intakeOn = false;
+  boolean noteIn = false;
+  double normalUpper = 0;
+  double normalLower = 0;
+  double upperSpeed = 5676;
+  double lowerSpeed = 6784;
+    
+
+  public static Shooter getInstance(String name) {
+    if(shooter == null) {
+      shooter = new Shooter();
+    }
+    System.out.println(name + " aquired Shooter");
+    return shooter;
+  }
+  private Shooter() {}
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command shoot() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          System.out.println("Shoot");
+          /* one-time action goes here */
+        });
+  }
+  
+  public void initializeShoot() {
+    started = false;
+    intakeOn = false;
+    noteIn = false;
+    normalUpper = 0;
+    normalLower = 0;
+    timer.reset();
+
+  }
+  public boolean runShooterMotors() {
+    upperShooterMotor.set(.5);
+    lowerShooterMotor.set(-.5);
+    if(!started) {
+      timer.reset();
+      timer.start();
+      started = true;
+    }
+    if(intakeOn) {
+      if( upperEncoder.getVelocity() - normalUpper < -10 || lowerEncoder.getVelocity() - normalLower < -10) {
+        noteIn = true;
+      }
+
+      if(noteIn) {
+        if(upperEncoder.getVelocity() >= normalUpper || lowerEncoder.getVelocity() >= normalUpper) {
+          return true;
+        }
+      }
+    }
+    if(timer.hasElapsed(1)) {
+      timer.stop();
+      normalUpper = upperEncoder.getVelocity();
+      normalLower = lowerEncoder.getVelocity();
+      intakeMotor.set(.5);
+      intakeOn = true;
+    }
+    return false;
+  }
+
+  public void resetMotors() {
+    intakeMotor.set(0);
+    upperShooterMotor.set(0);
+    lowerShooterMotor.set(0);
+  }
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
